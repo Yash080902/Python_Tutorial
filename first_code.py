4009fce6a93dd2559f12e51d8b9ffdb9e5b8ec98
Yash Kumar Goel <goelyash65@gmail.com>
2024-07-17 10:45:19 +0530

    yash has done

---
+++ first_code.py
@@ -0,0 +1,15 @@
+print('yash is hero of real world') # ye print el function hai or iske ander object pass kiya gaya hai, yaha pr string ko quotes mai hi dete hai
+print(10+19)
+print("Chalo chalte hai")
+# yaha pr ek sequence hi follow hoga lets see run, yaha pr ache se print hojayega sab cheeje 
+
+# `#`it is used to create comments
+print("Hi yas is here for your help anytime, \n this is next line")
+# \n  this is escaping character which is used to enter the line
+''' Yes I am here''', '''  Ye bhi comment hai jo multi line mai kaam aata hai
+BOSS Boss boss all good this is multi line Got it
+'''
+
+print('Yes we are writing the double quotes character in the "//yes", "yes"')
+# yaha pr humne single quote ka use kia jo hume ye btara hai hum single quotes k ander escape characters ko use kr skate hai without any error
+print("hey", 7, 9, sep="-", end="@#@") # Yaha pr we are lookig majorly the part of seerated by , sep ka matlab seperator jo , ko replace krdega sep k equivalent ko asani se
